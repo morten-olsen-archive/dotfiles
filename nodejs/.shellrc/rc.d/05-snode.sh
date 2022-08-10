4bf800f1ceac35707bdc3347a18bf90bd5cd41cd
Morten Olsen <morten+pgp@olsen.pro>
2022-08-10 23:36:53 +0200

    feat: added secure Node operations

---
+++ nodejs/.shellrc/rc.d/05-snode.sh
@@ -0,0 +1,4 @@
+alias yarn="snode.sh yarn"
+alias node="snode.sh node"
+alias npm="snode.sh npm"
+alias pnpm="snode.sh pnpm"
---
+++ scripts/.local/bin/snode.sh
@@ -0,0 +1,23 @@
+IMAGE="${DOCKER_IMAGE:-node:latest}"
+ROOT="$PWD"
+CWD="$PWD"
+ARGS="--rm -i"
+
+if git rev-parse --show-toplevel &> /dev/null; then
+  ROOT=`git rev-parse --show-toplevel`
+fi
+
+if [ -t 1 ]; then
+  ARGS="$ARGS -t"
+fi  
+
+docker \
+  --context default \
+  run \
+  $ARGS \
+  -e NPM_GITLAB_TOKEN="$NPM_GITLAB_TOKEN" \
+  --user "$UID:$GID" \
+  -v "$ROOT:$ROOT" \
+  --workdir="$CWD" \
+  "$IMAGE" \
+  $@
