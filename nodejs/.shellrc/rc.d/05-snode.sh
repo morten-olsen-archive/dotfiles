3626928e436fb0613b0194a90cd689b1e5cb7894
Morten Olsen <fbtijfdq@void.black>
2022-08-16 10:44:33 +0200

    fix: disabled snode.sh from aliases

--- nodejs/.shellrc/rc.d/05-snode.sh
+++ nodejs/.shellrc/rc.d/05-snode.sh
@@ -1,4 +1,4 @@
-alias yarn="snode.sh yarn"
-alias node="snode.sh node"
-alias npm="snode.sh npm"
-alias pnpm="snode.sh pnpm"
+#alias yarn="snode.sh yarn"
+#alias node="snode.sh node"
+#alias npm="snode.sh npm"
+#alias pnpm="snode.sh pnpm"
